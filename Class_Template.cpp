93765f3f2baebb87566746e872848e53a6727887
Luke <dgm.bailey@outlook.com>
2023-04-21 11:44:59 +0300

    Added a class template

---
+++ Class_Template.cpp
@@ -0,0 +1,20 @@
+// This is a template for every class / data type that I write
+
+class name
+{
+public:
+    name(/* arguments */);
+    ~name();
+private:
+    // Private Functions
+private:
+    // Private Data
+};
+
+name::name(/* arguments */)
+{
+}
+
+name::~name()
+{
+}
