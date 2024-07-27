4f6d9fe9b4d06a96aa5c651c72f681dea6b64e53
Hangarau <87149262+hangarau@users.noreply.github.com>
2024-07-27 17:12:36 +0100

    Create GetStarted.sh
    One command that lets you get started in C++ that's just too damn long for me to remember
    
    Laziness 100

---
+++ GetStarted.sh
@@ -0,0 +1,3 @@
+# How to get started in C++ with one command that's just too damn long for me to remember
+# Laziness 100
+mkdir dist dist/win dist/mac dist/tux src src/headers docs && touch Makefile README.md Development\ Notes.ipynb Documentation.md src/Main.cpp
